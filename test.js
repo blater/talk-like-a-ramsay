ad0ac2906264a5d8fb2da79253aad3aac301ea13
blater <blater@gmail.com>
2015-09-04 15:15:51 +0100

    feck

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ sinon.stub(Math, 'random').returns(0);
 assert.strictEqual(Math.random(), 0, 'stub worked');
 
 var text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when";
-var expected = "Lorem Ipsum is simply dummy text of the fucking printing and fucking typesetting industry. Lorem Ipsum has been the fucking industry's standard dummy text ev
+var expected = "Lorem Ipsum is simply dummy text of the fucking printing and fucking typesetting industry. Lorem Ipsum has been the fucking industry's standard dummy text ever since the fucking 1500s, when";
 
 describe('Fuck this', function() {
   it('should swear like a sailor', function(done) {
